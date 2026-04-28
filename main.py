f09ac0de91b6ed49af5c7407d2f6bb404b9b3582
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2026-04-28 13:13:29 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,118 @@
+from pathlib import Path
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+BASE_DIR = Path(__file__).resolve().parent
+STATIC_DIR = BASE_DIR / "static"
+TEMPLATES_DIR = BASE_DIR / "templates"
+# FastAPI 앱 초기화
+app = FastAPI(title="CHY College Homepage")
+
+# 정적 파일(CSS, JS, 이미지 등) 마운트
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory=str(TEMPLATES_DIR))
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="basic.html",
+        context={
+            "title": "CHYC - 사단법인 충효예대학",
+        },
+    )
+
+
+@app.get("/chymain", response_class=HTMLResponse)
+async def chymain(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/chy/mainpage.html",
+        context={
+            "title": "CHYC - 사단법인 충효예대학",
+        },
+    )
+
+
+@app.get("/greet", response_class=HTMLResponse)
+async def greet(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/chy/greetings.html",
+        context={
+            "title": "CHYC - 사단법인 충효예대학",
+        },
+    )
+
+@app.get("/advisors", response_class=HTMLResponse)
+async def advisors(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/chy/advisor.html",
+        context={
+            "title": "CHYC - 사단법인 충효예대학",
+        },
+    )
+
+@app.get("/map", response_class=HTMLResponse)
+async def map(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/chy/tmap.html",
+        context={
+            "title": "CHYC - 사단법인 충효예대학",
+        },
+    )
+
+@app.get("/edu_main", response_class=HTMLResponse)
+async def edumain(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/edu/edumain.html",
+        context={
+            "title": "CHYC - 사단법인 충효예대학",
+        },
+    )
+
+@app.get("/edu_chy", response_class=HTMLResponse)
+async def educhy(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/edu/edu_chy.html",
+        context={
+            "title": "CHYC - 사단법인 충효예대학",
+        },
+    )
+
+@app.get("/edu_cur", response_class=HTMLResponse)
+async def educur(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/edu/edu_cur.html",
+        context={
+            "title": "CHYC - 사단법인 충효예대학",
+        },
+    )
+
+@app.get("/edu_teachers", response_class=HTMLResponse)
+async def eduteachers(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/edu/edu_teachers.html",
+        context={
+            "title": "CHYC - 사단법인 충효예대학",
+        },
+    )
+
+
+@app.get("/gallery", response_class=HTMLResponse)
+async def gallery(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/edu/gallery.html",
+        context={
+            "title": "CHYC - 사단법인 충효예대학",
+        },
+    )
