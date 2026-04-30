bdb1e39c25fb126efa9e80dcd5ffd63ad77377e4
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2026-04-30 14:42:40 +0900

    메인페이지 변경

--- main.py
+++ main.py
@@ -123,3 +123,100 @@ async def gallery(request: Request):
         },
     )
 
+
+@app.get("/event_main", response_class=HTMLResponse)
+async def eventmain(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/event/eventmain.html",
+        context={
+            "title": "CHYC - 사단법인 충효예대학",
+        },
+    )
+
+
+@app.get("/event_01", response_class=HTMLResponse)
+async def event01(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/event/event01.html",
+        context={
+            "title": "CHYC - 사단법인 충효예대학",
+        },
+    )
+
+
+@app.get("/event_02", response_class=HTMLResponse)
+async def event02(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/event/event02.html",
+        context={
+            "title": "CHYC - 사단법인 충효예대학",
+        },
+    )
+
+@app.get("/event_03", response_class=HTMLResponse)
+async def event03(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/event/event03.html",
+        context={
+            "title": "CHYC - 사단법인 충효예대학",
+        },
+    )
+
+
+@app.get("/noble_main", response_class=HTMLResponse)
+async def noblemain(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/nobl/noble_main.html",
+        context={
+            "title": "CHYC - 사단법인 충효예대학",
+        },
+    )
+
+
+@app.get("/noble01", response_class=HTMLResponse)
+async def noble01(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/nobl/noble01.html",
+        context={
+            "title": "CHYC - 사단법인 충효예대학",
+        },
+    )
+
+
+@app.get("/utb", response_class=HTMLResponse)
+async def utb(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/utb.html",
+        context={
+            "title": "CHYC - 사단법인 충효예대학",
+        },
+    )
+
+
+@app.get("/jebomain", response_class=HTMLResponse)
+async def jebomain(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/jebomain.html",
+        context={
+            "title": "CHYC - 사단법인 충효예대학",
+        },
+    )
+
+
+@app.get("/jebo01", response_class=HTMLResponse)
+async def jebo01(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="/jebo01.html",
+        context={
+            "title": "CHYC - 사단법인 충효예대학",
+        },
+    )
